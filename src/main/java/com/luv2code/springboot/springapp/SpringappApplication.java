15f86f50fe556ebc87ce3eda1e1a63b65289a753
KDUTTA1 <KDUTTA1@L-185002504.wipro.com>
2019-05-09 14:18:12 +0530

    commit

--- src/main/java/com/luv2code/springboot/springapp/SpringappApplication.java
+++ src/main/java/com/luv2code/springboot/springapp/SpringappApplication.java
@@ -2,12 +2,9 @@ package com.luv2code.springboot.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@EnableFeignClients
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class SpringappApplication {
 
